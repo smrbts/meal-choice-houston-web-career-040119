38af2ff8fae5311491780f8367185af60eac6506
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-19 03:14:07 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,7 @@
+def meal_choice(choice = "meat")
+  puts "#{choice}"
+end
+
+meal_choice("vegan")
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
